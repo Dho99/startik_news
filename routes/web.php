6daa51a43e5aa6ee9d30c2454b2e9099302e0e65
Ridho A. <ridhoawwaludin@gmail.com>
2024-05-09 19:46:31 +0700

    Add Dashboard controller + middleware name in routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ViewController;
 
 // Route::get('/', function () {
@@ -12,4 +13,10 @@ Route::controller(ViewController::class)->middleware('web')->group(function(){
     Route::get('/','home');
     Route::get('/post','show');
     Route::get('/search','search');
+});
+
+Route::middleware('isAdmin')->group(function(){
+    Route::controller(DashboardController::class)->group(function(){
+        Route::get('/dashboard','index');
+    });
 });
